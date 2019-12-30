3033d775e5455f5364f49051c3738e82da4fcaa9
sabgilhun <sabgilhun@gmail.com>
2019-12-30 23:15:41 +0900

    json으로 다루기 위해 데이터 dict로 변경

--- python_script/bluesman_scraper.py
+++ python_script/bluesman_scraper.py
@@ -14,9 +14,13 @@ def scrap():
     soup = BeautifulSoup(request.text, 'html.parser')
 
     print(soup.encode('utf8'))
-    for item in soup.find_all('div', {'class': 'col xans-record-'}):
-        print(item.find('img', {'class': 'thumb_image'})['src'].replace("//", ""))
-        print(item.find('span', {'class': 'product_brand'}).text)
-        print(item.find('strong', {'class': 'price'}).text.replace("KRW ", ""))
-        print(item.find('div', {'class': 'product_title'}).find('span').text)
-        print('\n')
+    print(list(map(__to_json_parsable, soup.find_all('div', {'class': 'col xans-record-'}))))
+
+
+def __to_json_parsable(item):
+    json_parsable = dict()
+    json_parsable['image'] = item.find('img', {'class': 'thumb_image'})['src'].replace("//", "")
+    json_parsable['brand'] = item.find('span', {'class': 'product_brand'}).text
+    json_parsable['price'] = item.find('strong', {'class': 'price'}).text.replace("KRW ", "")
+    json_parsable['product'] = item.find('div', {'class': 'product_title'}).find('span').text
+    return json_parsable
