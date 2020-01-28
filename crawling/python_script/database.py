b77554dde95196dd6da218826cf28dc19866671e
sabgilhun <sabgilhun@gmail.com>
2020-01-28 19:18:01 +0900

    기존 파베 코드 제거

--- crawling/python_script/database.py
+++ crawling/python_script/database.py
@@ -1,27 +1,75 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+import pymysql
+from _datetime import datetime
 
-scrapped_database_path = 'scrapped_data/'
-post_database_path = 'post_data/'
+item_insert_sql = """
+        INSERT INTO item(shop_name,product_name,brand,image_url,page_url,price,post_id)
+                 VALUES (%s, %s, %s, %s, %s, %s, %s)
+                 """
 
+post_insert_sql = """
+        INSERT INTO post(shop_name,shop_logo_url,upload_date)
+                 VALUES (%s, %s, %s)
+                 """
 
-def init():
-    cred = credentials.Certificate("../firebase_key.json")
-    firebase_admin.initialize_app(cred, {'databaseURL': 'https://contena-5c99b.firebaseio.com'})
-    print('complete db initialize')
+item_select_sql = """
+        SELECT shop_name,product_name,brand,image_url,page_url,price FROM item WHERE shop_name=%s
+        """
 
-
-def get_scrapped_items(shop_name):
-    ref = db.reference(scrapped_database_path + shop_name)
-    return ref.get()
-
-
-def set_scrapped_items(shop_name, data):
-    ref = db.reference(scrapped_database_path + shop_name)
-    ref.set(data)
+post_select_sql = """
+        SELECT id FROM post WHERE shop_name=%s ORDER BY id DESC
+        """
 
 
-def push_post(post):
-    ref = db.reference(post_database_path)
-    ref.push(post)
+def connect_contena_db():
+    return pymysql.connect(host='db', user='root', password='1234', db='contena', charset='utf8',
+                           cursorclass=pymysql.cursors.DictCursor)
+
+
+# return pymysql.connect(host='127.0.0.1', port=3306, user='root', password='1234', db='contena', charset='utf8',
+#                        cursorclass=pymysql.cursors.DictCursor)
+
+
+def insert_item(cursor, items, post_id):
+    for item in items:
+        cursor.execute(item_insert_sql, (item['shop_name'], item['product_name'], item['brand'],
+                                         item['image_url'], item['page_url'], item['price'], post_id))
+
+
+def insert_post(cursor, post):
+    cursor.execute(post_insert_sql, (post['shop_name'], post['shop_logo_url'], post['upload_date']))
+
+
+def select_one_post_id_with_shop_name(shop_name, cursor=None):
+    if not cursor:
+        cursor = connect_contena_db().cursor()
+
+    cursor.execute(post_select_sql, (shop_name,))
+    return cursor.fetchone()
+
+
+def select_item_with_shop_name(shop_name, cursor=None):
+    if not cursor:
+        cursor = connect_contena_db().cursor()
+
+    cursor.execute(item_select_sql, (shop_name,))
+    return cursor.fetchall()
+
+
+def begin_item_and_post_insert(new_items, shop_logo_url):
+    shop_name = new_items[0]['shop_name']
+
+    post = dict()
+    post['shop_name'] = shop_name
+    post['shop_logo_url'] = shop_logo_url
+    post['upload_date'] = datetime.strftime(datetime.now(), '%y-%m-%d %H:%M')
+
+    connection = connect_contena_db()
+
+    try:
+        with connection.cursor() as cursor:
+            insert_post(cursor, post)
+            post_id = select_one_post_id_with_shop_name(shop_name, cursor)['id']
+            insert_item(cursor, new_items, post_id)
+            connection.commit()
+    finally:
+        connection.close()
--- crawling/python_script/main.py
+++ crawling/python_script/main.py
@@ -1,7 +1,7 @@
 from _datetime import datetime
 from scraping.scraping_handler import *
 from model_logic.new_arrival import *
-import new_database
+import database
 
 if __name__ == '__main__':
     time_stamp = datetime.strftime(datetime.now(), '%y-%m-%d %H:%M')
@@ -11,9 +11,9 @@ if __name__ == '__main__':
         shop_name = scraping_data['shop_name']
         shop_logo = scraping_data['shop_logo']
         new_scrapped_items = scraping_data['scrapped_item']
-        old_scrapped_items = new_database.select_item_with_shop_name(shop_name)
+        old_scrapped_items = database.select_item_with_shop_name(shop_name)
 
         new_arrival_items = extract_new_arrival_item(old_scrapped_items, new_scrapped_items)
         if new_arrival_items:
             print("new items!!")
-            new_database.begin_item_and_post_insert(new_arrival_items, shop_logo)
+            database.begin_item_and_post_insert(new_arrival_items, shop_logo)
