acd5cff313f37d6aa59b9a4e9f82745a994f7749
sabgilhun <sabgilhun@gmail.com>
2020-01-29 14:03:13 +0900

    jpa, mysql 의존성 추가

--- rest-api/build.gradle.kts
+++ rest-api/build.gradle.kts
@@ -18,10 +18,13 @@ repositories {
 dependencies {
     implementation("com.google.firebase:firebase-admin:6.8.1")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("mysql:mysql-connector-java")
+    runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
