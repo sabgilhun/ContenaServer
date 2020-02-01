1e861b6205f0df5fe21da828b4874a142d5077c7
sabgilhun <sabgilhun@gmail.com>
2020-02-01 23:40:51 +0900

    엔티티에 data 클래스 제거

--- rest-api/src/main/kotlin/com/sabgil/contena/entitiy/Item.kt
+++ rest-api/src/main/kotlin/com/sabgil/contena/entitiy/Item.kt
@@ -5,44 +5,30 @@ import javax.validation.constraints.NotBlank
 
 
 @Entity
-data class Item(
+class Item(
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
         var id: Long = 0,
 
         @Column(name = "shop_name")
-        @get: NotBlank
         var shopName: String = "",
 
         @Column(name = "product_name")
-        @get: NotBlank
         var productName: String = "",
 
         @Column(name = "brand")
-        @get: NotBlank
         var brand: String = "",
 
         @Column(name = "image_url")
-        @get: NotBlank
         var imageUrl: String = "",
 
         @Column(name = "page_url")
-        @get: NotBlank
         var pageUrl: String = "",
 
         @Column(name = "price")
-        @get: NotBlank
         var price: String = "",
 
         @ManyToOne
-        @get: NotBlank
         @JoinColumn(name = "post_id")
         var post: Post? = null
-) {
-    override fun toString(): String {
-        return "Item(id=$id, shopName='$shopName', productName='$productName'," +
-                " brand='$brand', imageUrl='$imageUrl', pageUrl='$pageUrl', price='$price')"
-    }
-}
-
-
+)
