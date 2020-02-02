6bdf9bca5da79c4474c2151e4e4cbfa7aea2f409
sabgilhun <sabgilhun@gmail.com>
2020-02-02 16:50:03 +0900

    유저가 구독한 쇼핑몰 리스트 api 구현

--- rest-api/src/main/kotlin/com/sabgil/contena/controller/ShopController.kt
+++ rest-api/src/main/kotlin/com/sabgil/contena/controller/ShopController.kt
@@ -1,11 +1,17 @@
 package com.sabgil.contena.controller
 
+import com.sabgil.contena.repository.ShopRepository
+import com.sabgil.contena.repository.SubscriptionRepository
+import com.sabgil.contena.response.shop.GetShopListResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ShopController {
+class ShopController(
+        private val shopRepository: ShopRepository,
+        private val subscriptionRepository: SubscriptionRepository
+) {
 
     @GetMapping("/shop_list")
     fun getAvailableShopList(
@@ -17,7 +23,11 @@ class ShopController {
     @GetMapping("/shop_list")
     fun getSubscriptionShopList(
             @RequestParam(value = "user_id", defaultValue = "") userId: String
-    ) {
-        TODO()
+    ): GetShopListResponse {
+        val shopEntities = subscriptionRepository.findByUserId(userId)
+                .filter { it.shopEntity != null }
+                .map { it.shopEntity!! }
+
+        return GetShopListResponse.from(shopEntities)
     }
 }
