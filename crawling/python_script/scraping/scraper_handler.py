ddc6bced78b1c64a877029249adc0ede28adca0c
sabgilhun <sabgilhun@gmail.com>
2020-02-03 22:03:29 +0900

    mode man 사이트 추가

--- crawling/python_script/scraping/scraper_handler.py
+++ crawling/python_script/scraping/scraper_handler.py
@@ -1,10 +1,15 @@
 from . import bluesman_scraper
+from . import modeman_scraper
 
 
 def new_arrival_data_generator_with_scraping():
     yield {'shop_name': bluesman_scraper.shop_name,
            'shop_logo_url': bluesman_scraper.scrap_shop_logo(),
            'scrapped_item': bluesman_scraper.scrap_new_arrival_item()
+           }
+    yield {'shop_name': modeman_scraper.shop_name,
+           'shop_logo_url': modeman_scraper.scrap_shop_logo(),
+           'scrapped_item': modeman_scraper.scrap_new_arrival_item()
            }
     # yield {'shop_name': '',
     #       'shop_logo': logo,
