ad60829a6149bb16e4ba6058ad51b2220e880a78
sabgilhun <sabgilhun@gmail.com>
2020-02-29 18:35:21 +0900

    Notification Controller 추가

---
+++ rest-api/src/main/kotlin/com/sabgil/contena/controller/NotificationController.kt
@@ -0,0 +1,35 @@
+package com.sabgil.contena.controller
+
+import com.sabgil.contena.exceptiom.BadRequestException
+import com.sabgil.contena.repository.ShopRepository
+import com.sabgil.contena.request.notification.NotiNewItemRequest
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class NotificationController(
+        private val shopRepository: ShopRepository
+) {
+
+    @PostMapping("/notification/new_item")
+    fun postNotificationNewItem(
+            @RequestBody notiNewItemRequest: NotiNewItemRequest
+    ) {
+        if (notiNewItemRequest.shopList == null) {
+            throw BadRequestException("갱신된 쇼핑몰 리스트가 비어 있습니다.")
+        }
+
+        val hasToPushMessageUserList =
+                notiNewItemRequest.shopList!!.fold(mutableListOf<String>()) { userTokenList, shopName ->
+                    val shopEntity = shopRepository.findByShopName(shopName)
+
+                    if (shopEntity != null) {
+                        userTokenList.addAll(shopEntity.subscriptionEntities?.map { it.userId }?.toList()
+                                ?: emptyList())
+                    }
+
+                    return@fold userTokenList
+                }
+    }
+}
